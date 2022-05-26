a835d7e8dba1a3b1e07a1be2ea38d23e9d8407ce
Bodea Denis <denis_gabriel94@yahoo.com>
2022-05-26 15:54:09 +0300

    all code

--- src/app.js
+++ src/app.js
@@ -11,6 +11,29 @@ app.use(express.json());
 
 app.use('/api', router);
 
+app.get('/', (req, res) => {
+  res.render('index', { name: 'Denis' });
+});
+
+app.get('/users', (req, res) => {
+  const users = [
+    {
+      name: 'Denis',
+      age: 23
+    },
+    {
+      name: 'John',
+      age: 21
+    }
+  ];
+  res.render('users', { name: 'Denis', users });
+});
+
+// 404 page 
+app.use((req, res) => {
+  res.status(404).render('404');
+});
+
 app.listen(CONFIG.PORT || 5000, () => {
   console.log("Express app is listening on port " + CONFIG.PORT || 5000);
 });
